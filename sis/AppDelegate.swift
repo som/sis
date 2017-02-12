7fc7cd32bc3ff16a9e0153d714d4b53cfe06fe5d
Sergey Tupicin <STupicin@gmail.com>
2017-02-12 22:05:12 +0300

    Initial Commit

---
+++ sis/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sis
+//
+//  Created by som on 12/02/2017.
+//  Copyright © 2017 som. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sis
+//
+//  Created by som on 12/02/2017.
+//  Copyright © 2017 som. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
