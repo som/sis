818d81bd6594ac01b4b7b09e2aec32b166bfd5ec
Sergey Tupicin <STupicin@gmail.com>
2017-02-13 11:15:23 +0300

    remove keyFlag

--- sis/AppDelegate.swift
+++ sis/AppDelegate.swift
@@ -17,7 +17,6 @@ let cmdRight:UInt = 1048848
 
 
 var flagCmd:UInt = 0
-var flagKey:Int = 0
 func cbFlags(event:NSEvent){
     if (event.modifierFlags.contains(NSEventModifierFlags.command)){
         if (flagCmd == 0){
@@ -26,12 +25,10 @@ func cbFlags(event:NSEvent){
             flagCmd = 0;
         }
     }else{
-        if (flagKey == 0){
-            if (flagCmd == cmdLeft){
-                TISSelectInputSource( aKeyInputSources![0] )
-            }else if (flagCmd == cmdRight){
-                TISSelectInputSource( aKeyInputSources![1] )
-            }
+        if (flagCmd == cmdLeft){
+            TISSelectInputSource( aKeyInputSources![0] )
+        }else if (flagCmd == cmdRight){
+            TISSelectInputSource( aKeyInputSources![1] )
         }
         flagCmd = 0;
     }
@@ -39,13 +36,11 @@ func cbFlags(event:NSEvent){
 }
 
 func cbDown(event:NSEvent){
-    if (!event.isARepeat){
-        flagKey += 1
-    }
+    flagCmd = 0;
 }
 
 func cbUp(event:NSEvent){
-    flagKey -= 1
+    flagCmd = 0;
 }
 
 func getProperty<T>(_ source: TISInputSource, _ key: CFString) -> T? {
